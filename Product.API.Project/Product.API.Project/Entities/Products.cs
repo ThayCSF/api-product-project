978fb56acd2a20dbf20bdedbf56f38ed50896644
Thayna Cristina Da Silva Ferreira <thayna.ferreira@juntossomosmais.com.br>
2022-03-09 18:18:02 -0300

    Controller code updates

--- Product.API.Project/Product.API.Project/Entities/Products.cs
+++ Product.API.Project/Product.API.Project/Entities/Products.cs
@@ -3,9 +3,9 @@
     public class Products
     {
         public long Id { get; private set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public decimal UnitValue { get; set; }
-        public string Seller { get; set; }
+        public string? Seller { get; set; }
         public DateTime CreatedAt { get; private set; }
         public DateTime UpdatedAt { get; set; }
         public bool IsActive { get; set; }
