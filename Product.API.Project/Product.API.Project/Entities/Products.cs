d302d29f811cdd0e8f14cc6c43665fc6e4f00d26
Thayna Cristina Da Silva Ferreira <thayna.ferreira@juntossomosmais.com.br>
2022-03-09 16:38:03 -0300

    Adding initial archives

---
+++ Product.API.Project/Product.API.Project/Entities/Products.cs
@@ -0,0 +1,22 @@
+﻿namespace Product.API.Project.Entities
+{
+    public class Products
+    {
+        private long Id { get; set; }
+        public string Name { get; set; }
+        public decimal UnitValue { get; set; }
+        public string Seller { get; set; }
+        public int Quantity { get; set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime UpdatedAt { get; set; }
+        public bool IsActive { get; set; }
+
+        public Products()
+        {
+            var now = DateTime.Now;
+            CreatedAt = now;
+            UpdatedAt = now;
+            IsActive = true;
+        }
+    }
+}
---
+++ Product.API.Project/Product.API.Project/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using Product.API.Project.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<ProductContext>(opt =>
+    opt.UseInMemoryDatabase("ProductDb"));
+//builder.Services.AddSwaggerGen(c =>
+//{
+//    c.SwaggerDoc("v1", new() { Title = "TodoApi", Version = "v1" });
+//});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    //app.UseSwagger();
+    //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoApi v1"));
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
