bdf376ac54c0c813f6bc52f72f87832831c69f19
Thayna Cristina Da Silva Ferreira <thayna.ferreira@juntossomosmais.com.br>
2022-03-10 18:36:21 -0300

    Solving database issues

--- Product.API.Project/Product.API.Project/Entities/Products.cs
+++ Product.API.Project/Product.API.Project/Entities/Products.cs
@@ -6,16 +6,6 @@
         public string? Name { get; set; }
         public decimal UnitValue { get; set; }
         public string? Seller { get; set; }
-        public DateTime CreatedAt { get; private set; }
-        public DateTime UpdatedAt { get; set; }
-        public bool IsActive { get; set; }
-
-        public Products()
-        {
-            var now = DateTime.Now;
-            CreatedAt = now;
-            UpdatedAt = now;
-            IsActive = true;
-        }
+       
     }
 }
